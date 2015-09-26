6ce3cdd20d755a5f2751436c0e6ffb98bda2ce01
GeorgeValergas <gvalergas@gmail.com>
2015-09-26 15:59:51 -0400

    fix test path

--- tests/simplify_test.js
+++ tests/simplify_test.js
@@ -1,40 +1,40 @@
-var keys = require("./keys");
+var keys = require("../keys");
 var Simplify = require("simplify-commerce"),
 client = Simplify.getClient({
     publicKey: keys.public,
-    privateKey: peys.private
+    privateKey: keys.private
 });
-// console.log(client);
-// client.payment.create({
-//     amount : "1",
-//     description : "payment description",
-//     card : {
-//        expMonth : "11",
-//        expYear : "19",
-//        cvc : "123",
-//        number : "5555555555554444"
-//     },
-//     currency : "USD"
-// }, function(errData, data){
-//     if(errData){
-//         console.log(errData);      
-//         // console.error("Error Message: " + errData.data.error.message);
-//         // handle the error
-//         return;
-//     }
-//     console.log("Payment Status: " + data.paymentStatus);
-// });
-
+console.log(keys);
 client.payment.create({
-    amount : "1000",
-    token : "f21da65e-f0ab-45cb-b8e6-40b493c3671f",
+    amount : "100",
     description : "payment description",
+    card : {
+       expMonth : "11",
+       expYear : "19",
+       cvc : "123",
+       number : "5555555555554444"
+    },
     currency : "USD"
 }, function(errData, data){
     if(errData){
-        console.error("Error Message: " + errData.data.error.message);
+        console.log(errData);      
+        // console.error("Error Message: " + errData.data.error.message);
         // handle the error
         return;
     }
     console.log("Payment Status: " + data.paymentStatus);
 });
+
+// client.payment.create({
+//     amount : "1000",
+//     token : "f21da65e-f0ab-45cb-b8e6-40b493c3671f",
+//     description : "payment description",
+//     currency : "USD"
+// }, function(errData, data){
+//     if(errData){
+//         console.error("Error Message: " + errData.data.error.message);
+//         // handle the error
+//         return;
+//     }
+//     console.log("Payment Status: " + data.paymentStatus);
+// });
