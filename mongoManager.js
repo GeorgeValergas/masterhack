a4aafb284afc889ed84a18d7f819d94647ae4a04
Mark Karanfil <m.karanf@gmail.com>
2015-09-26 18:59:59 -0400

    Added MongoDB CRUD operation to modify the goal amount of a goal specified by name.

--- mongoManager.js
+++ mongoManager.js
@@ -19,9 +19,25 @@ module.exports = {
     })
   },
 
-  addParticipantToGoal: function(goalObject, participant) {
-  	
+  addParticipantToGoal: function(goalName, participantObject, participant) {
+
   },
+
+  changeGoalAmount: function(goalName, goalAmount) {
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, db) {
+      // Insert a single participant to the participants list
+      db.collection('goals').update({"name": goalName}, { $set: {"goalAmount": goalAmount}}, function(err, results) {
+          // Fetch the document that we modified
+          db.collection('goals').findOne({"name": goalName}, function(err, item) {
+            assert.equal(null, err);
+            assert.equal(goalAmount, item.goalAmount);
+            db.close();
+          });
+        });
+    })
+  },
+
 
   removeParticipantFromGoal: function(goalObject, participant) {
   	
--- tests/mongo_test.js
+++ tests/mongo_test.js
@@ -1,7 +1,8 @@
 var mongoManager = require("../mongoManager.js");
+var goalName = "Buying car for Chantale";
 
 var goal = {
-  "name":"Buying car for Chantale",
+  "name": goalName,
   "goalAmount": 50000,
   "active": true, //If this is FALSE, the idea is cancelled
   "complete": false, //If this is TRUE, the payment was captured from everyone. If FALSE, it is still only authorized
@@ -24,4 +25,11 @@ var goal = {
   ]
 };
 
+var participant = {
+	"email" : "daniel@concordia.ca",
+  	"amount" : 1000,
+	"paid": true
+}
+
 mongoManager.insertGoal(goal);
+mongoManager.changeGoalAmount(goalName, 20000);
